b80998feee4674f1e97687eac5bda412b2f3e5b1
Springo <kevinmxia@gmail.com>
2016-02-18 23:37:45 -0500

    changed input style

--- SpringoCipher.c
+++ SpringoCipher.c
@@ -10,21 +10,28 @@
 #define SIZE 94
 
 int main(int argv, char** argc) {
-	if (argv != 4 || strlen(argc[1]) > 1 || !(*argc[1] == 'e' || *argc[1] == 'd')) {
-		printf("Format: SpringoCipher <e/d> <message> <key>\n");
+	if (argv != 2 || strlen(argc[1]) > 1 || !(*argc[1] == 'e' || *argc[1] == 'd')) {
+		printf("Format: SpringoCipher <e/d>\n");
 		printf("e: encrypt, d: decrypt\n");
-		printf("Use backslashes to escape spaces. (E.g. Hello\\ World)\n");
 		return 0;
 	}
-	char* output = "ERROR";
+	char* output;
+	char* message = (char*)malloc(500);
+	char* key = (char*)malloc(100);
+	printf("Enter message: ");
+	scanf("%[^\n]", message);
+	getchar();
+	printf("Enter key: ");
+	scanf("%[^\n]", key);
+	getchar();
 	if (*argc[1] == 'e') {
-		if ((output = encryptText(argc[2], argc[3])) == NULL) {
+		if ((output = encryptText(message, key)) == NULL) {
 			printf("Error");
 			return 1;
 		}
 	}
 	else {
-		if ((output = decryptText(argc[2], argc[3])) == NULL) {
+		if ((output = decryptText(message, key)) == NULL) {
 			printf("Error");
 			return 1;
 		}
